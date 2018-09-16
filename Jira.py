mrozekma/standup

commit 114cb8016071fbab379d0f642b3c1d63fbbfa6eb

Issue assignment

Jira.py
@@ -60,20 +60,26 @@ class Jira:
 		if self.cache is None:
 			cacheRead = cacheWrite = False
 
-		if cacheRead:
-			rtn = self.cache[(route, params)]
-			if rtn is not None:
-				return rtn
-
-		console('jira api', f"API request: {route} {params} {data}")
-		# import sys, traceback
-		# traceback.print_stack(file = sys.__stdout__)
 		namespace, rest = route.split('/', 1)
 		try:
 			version = apiVersions[namespace]
 		except KeyError:
 			raise ValueError(f"Unknown API namespace: {namespace}")
 		url = f"{config.jiraUrl}/rest/{namespace}/{version}/{rest}"
+		return self.load(method, url, cacheRead = cacheRead, cacheWrite = cacheWrite, data = data, **params)
+
+	def load(self, method, url, *, cacheRead = False, cacheWrite = None, data = None, **params):
+		if cacheWrite is None:
+			cacheWrite = (method == 'get')
+		if self.cache is None:
+			cacheRead = cacheWrite = False
+
+		if cacheRead:
+			rtn = self.cache[url]
+			if rtn is not None:
+				return rtn
+
+		console('jira api', f"API request: {url} {params} {data}")
 		req = requests.request(
 			method,
 			url,
@@ -99,24 +105,25 @@ class Jira:
 			raise APIError(req.status_code, message, url)
 
 		rtn = req.json()
-		if 'startAt' in rtn:
+		if method == 'get' and 'startAt' in rtn:
 			# Result is paginated
 			if 'startAt' in params:
 				# This is one page of a paginated request being called from paginate(), just return it
 				return rtn
 			else:
 				# This is the beginning of a paginated request
-				return self.paginate(route, params, rtn, cacheWrite)
+				return self.paginate(url, params, rtn, cacheWrite)
 		else:
 			# Result is not paginated, just return the whole thing
 			if cacheWrite:
-				self.cache[(route, params)] = rtn
+				self.cache[url] = rtn
 			return rtn
 
 	get = partialmethod(request, 'get')
 	post = partialmethod(request, 'post', cacheRead = False, cacheWrite = False)
+	put = partialmethod(request, 'put', cacheRead = False, cacheWrite = False)
 
-	def paginate(self, route, params, firstPage, cacheWrite):
+	def paginate(self, url, params, firstPage, cacheWrite):
 		# First, determine the key the actual values are stored at.
 		# It's supposed to be 'values'
 		if 'values' in firstPage:
@@ -135,13 +142,13 @@ class Jira:
 		page = firstPage
 		# Sometimes 'total' isn't in the result, even though Atlassian claims it will be. I think this happens when Jira finishes caching a result and it becomes no longer paginated even though it started that way
 		while len(page[key]) > 0 and ('total' not in page or len(wholeList) < page['total']):
-			page = self.get(route, startAt = page['startAt'] + len(page[key]), **params)
+			page = self.load('get', url, startAt = page['startAt'] + len(page[key]), **params)
 			wholeList += page[key]
 			yield from page[key]
 
 		# Cache the entire result
 		if cacheWrite:
-			self.cache[(route, params)] = wholeList
+			self.cache[url] = wholeList
 
 	def getProjects(self):
 		convertDate = lambda ts: self.parseTimestamp(ts).strftime('%d %b').lstrip('0')

HTTPHandler.py
@@ -47,8 +47,8 @@ class HTTPHandler(ParentHandler):
 			# self.log = False
 
 		if user:
-			# Clear Jira cache on hard refresh. Only count hard refreshes of view-backed pages, since some normal requests are sent no-cache (e.g. favicon)
-			if 'view' in handler and self.headers.get('Cache-Control', None) == 'no-cache':
+			# Clear Jira cache on hard refresh. Exclude static loads because some are sent no-cache (e.g. favicon)
+			if handler.get('name', None) != 'static' and self.headers.get('Cache-Control', None) == 'no-cache':
 				self.session['jiraCache'].clear()
 		elif 'allowGuest' not in handler or not handler['allowGuest']:
 			print("<su-login></su-login>")

handlers/sprint.py
@@ -55,6 +55,28 @@ def getParentIssues(jira, issues, existingIds = None):
 			existingIds.add(parent['id'])
 			yield from getParentIssues(jira, [parent], existingIds)
 
+def getMembers(jira, sprintId):
+	# This is ridiculously complicated:
+
+	# Get the board from the sprint
+	boardId = jira.get(f"agile/sprint/{sprintId}", cacheRead = True)['originBoardId']
+	# Get the projects from the board
+	projectIds = [project['id'] for project in jira.get(f"agile/board/{boardId}/project", cacheRead = True)]
+	# Get the roles from the projects
+	roleUrls = [url for projectId in projectIds for name, url in jira.get(f"api/project/{projectId}", cacheRead = True)['roles'].items()]
+	# Get the actors from the roles
+	actors = [actor for url in roleUrls for actor in jira.load('get', url, cacheRead = True)['actors']]
+	# Get users from the actors ({username: avatarUrl})
+	directUsers = {actor['name']: actor['avatarUrl'] for actor in actors if actor['type'] == 'atlassian-user-role-actor'}
+	# Get group names from the actors
+	groups = {actor['name'] for actor in actors if actor['type'] == 'atlassian-group-role-actor'}
+	# Get users from the groups
+	indirectUsers = {user['name']: jira.getLargestAvatar(user['avatarUrls']) for groupname in groups for user in jira.get("api/group", groupname = groupname, expand = 'users', cacheRead = True)['users']['items']}
+
+	users = {**directUsers, **indirectUsers}
+	userList = [{'username': name, 'avatar': avatar} for (name, avatar) in users.items()]
+	return sorted(userList, key = lambda user: user['username'])
+
 @get('sprint/(?P<id>[0-9]+)', view = 'sprint')
 def sprint(handler, id):
 	handler.title(False)
@@ -62,6 +84,8 @@ def sprint(handler, id):
 
 @get('sprint/(?P<id>[0-9]+)/data')
 def sprintData(handler, id):
+	handler.wrappers = False
+
 	try:
 		data = list(handler.jira.get(f"agile/sprint/{id}/issue", expand = 'transitions'))
 		issues = list(processIssue(handler.jira, issue) for issue in data)
@@ -74,17 +98,15 @@ def sprintData(handler, id):
 		else:
 			sprint = handler.jira.get(f"agile/sprint/{id}")
 
-		rtn = {'sprint_name': sprint['name'], 'issues': issues, 'parents': parents}
+		rtn = {'sprint_name': sprint['name'], 'members': getMembers(handler.jira, id), 'issues': issues, 'parents': parents}
+		print(json.dumps(rtn))
+		handler.contentType = 'application/json'
 	except APIError as e:
-		rtn = str(e)
+		print(str(e))
 		handler.responseCode = 400
 
-	print(json.dumps(rtn))
-	handler.wrappers = False
-	handler.contentType = 'application/json'
-
 @post('sprint/(?P<id>[0-9]+)/update')
-def sprintUpdate(handler, id, p_issue, p_transition = None):
+def sprintUpdate(handler, id, p_issue, p_transition = None, p_assignee = None):
 	handler.wrappers = False
 	p_issue = int(p_issue)
 
@@ -94,9 +116,11 @@ def sprintUpdate(handler, id, p_issue, p_transition = None):
 			data = handler.jira.get(f"api/issue/{p_issue}/transitions")
 			print(json.dumps(formatTransitions(data['transitions'])))
 			handler.contentType = 'application/json'
+		elif p_assignee is not None:
+			handler.jira.put(f"api/issue/{p_issue}", data = {'fields': {'assignee': {'name': str(p_assignee)}}})
 		else:
 			handler.responseCode = 400
 			print("No update")
-	except APIError as e:
+	except BaseException as e:
 		handler.responseCode = 400
 		print(str(e))

views/sprint.view
@@ -14,6 +14,7 @@
             <th>Summary</th>
             <th>Assignee</th>
             <th>Hours</th>
+            <th></th>
         </tr>
         <tr v-if="!loaded" class="loading-row">
             <td colspan="6">
@@ -36,22 +37,32 @@
             <td class="key">{{ issue.key }}</td>
             <!--<td class="expander"></td>-->
             <td class="summary" :style="`padding-left: ${issue.indent * 30}px;`"><img :src="issue.type.icon" :title="issue.type.name" class="type"> {{ issue.summary }}</td>
-            <td v-if="issue.assignee" class="assignee">
+            <td v-if="issue.assignee" class="assignee" @click.prevent="$refs.assignee_menu[idx].open">
                 <img :src="issue.assignee.avatar" class="avatar">
                 <div class="username">{{ issue.assignee.username }}</div>
             </td>
-            <td v-else class="assignee">
+            <td v-else class="assignee" @click.prevent="$refs.assignee_menu.open">
                 Unassigned
             </td>
             <td class="hours">
                 <vue-simple-progress text-position="top" :text="formatSeconds(issue.remaining)" :val="Math.max(0, issue.estimate - issue.remaining)" :max="issue.estimate"></vue-simple-progress>
             </td>
+            <td class="links">
+                <a :href="`${$global.jiraUrl}/browse/${issue.key}`" title="View on Jira" target="_blank"><img :src="`${$global.jiraUrl}/images/64jira.png`"></a>
+            </td>
         </tr>
     </table>
 
-    <vue-context v-for="issue in issuesOrdered" :key="issue.id" ref="status_menu">
+    <vue-context v-for="issue in issuesOrdered" :key="`status-menu-${issue.id}`" ref="status_menu">
         <ul>
             <li v-for="transition in issue.transitions" @click="doTransition(issue, transition)"><img :src="`/static/images/status-${transition.status.category}.png`" :title="transition.status.name">&nbsp;{{ transition.name }}</li>
+        </ul>
+    </vue-context>
+
+    <!--TODO Should be possible to only have one menu, but need to figure out which issue was clicked -->
+    <vue-context v-for="issue in issuesOrdered" :key="`assignee-menu-${issue.id}`" ref="assignee_menu" class="assignee-menu">
+        <ul>
+            <li v-for="member in members" @click="doAssign(issue, member)"><img :src="member.avatar">{{ member.username }}</li>
         </ul>
     </vue-context>
 </view>
@@ -71,6 +82,7 @@
             loaded: false,
             error: null,
             sprint_name: null,
+            members: [],
             issues: [],
             parents: [],
             processing: [], // issues currently processing. Sets aren't reactive :(
@@ -123,6 +135,7 @@
                     success: function(data) {
                         self.error = null;
                         self.sprint_name = data.sprint_name;
+                        self.members = data.members;
                         self.issues = data.issues;
                         self.parents = data.parents;
                         self.processing.splice(0, self.processing.length);
@@ -205,7 +218,16 @@
                         issue.transitions = data;
                     },
                 );
-            }
+            },
+            doAssign: function(issue, user) {
+                this.doUpdate(
+                    issue,
+                    {assignee: user.username},
+                    function(data) {
+                        issue.assignee = user;
+                    },
+                );
+            },
         }
     }
 </script>
@@ -260,6 +282,11 @@
             td {
                 white-space: nowrap;
                 padding: 4px 1px;
+
+                img {
+                    position: relative;
+                    top: -1px;
+                }
             }
 
             .index {
@@ -300,6 +327,13 @@
                 /*.estimate, .remaining {*/
                 /*display: inline-block;*/
                 /*}*/
+            }
+
+            .links {
+                padding: 0 10px;
+                img {
+                    width: 16px;
+                }
             }
         }
     }
@@ -356,7 +390,19 @@
             padding: 0 !important;
             li {
                 padding: 10px !important;
+                img {
+                    position: relative;
+                    top: -2px;
+                }
+            }
+        }
+
+        &.assignee-menu {
+            img {
+                width: 16px;
+                margin-right: 3px;
             }
         }
     }
+
 </style>

commit 7cdf394b54e4e7f2ae03853d7993f7ecb95e24fb

Unbreak the caching I broke in 114cb8016071fbab379d0f642b3c1d63fbbfa6eb

Jira.py
@@ -75,7 +75,7 @@ class Jira:
 			cacheRead = cacheWrite = False
 
 		if cacheRead:
-			rtn = self.cache[url]
+			rtn = self.cache[(url, params, data)]
 			if rtn is not None:
 				return rtn
 
@@ -116,7 +116,7 @@ class Jira:
 		else:
 			# Result is not paginated, just return the whole thing
 			if cacheWrite:
-				self.cache[url] = rtn
+				self.cache[(url, params, data)] = rtn
 			return rtn
 
 	get = partialmethod(request, 'get')
@@ -148,7 +148,7 @@ class Jira:
 
 		# Cache the entire result
 		if cacheWrite:
-			self.cache[url] = wholeList
+			self.cache[(url, params, None)] = wholeList
 
 	def getProjects(self):
 		convertDate = lambda ts: self.parseTimestamp(ts).strftime('%d %b').lstrip('0')